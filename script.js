d7624caa1c8666bb65affbd8be45ca4e5cbb68e0
AbidKhairyAK <abid.khairy.ak@gmail.com>
2021-07-25 20:42:30 +0700

    init

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener('alpine:init', () => {
+	Alpine.data('main', () => ({
+		students: [
+			{ name: 'muji', projects: [
+				{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
+			]}, 
+			{ name: 'abdu', projects: [
+				{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
+			]}, 
+			{ name: 'fatra', projects: [
+				{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
+			]}, 
+			{ name: 'hudzaifah', projects: [
+				{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
+			]}, 
+			{ name: 'ramadhan', projects: [
+				{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
+			]}, 
+			{ name: 'ihsan', projects: [
+				{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
+			]}, 
+			{ name: 'asep', projects: [
+				{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
+			]},
+		],
+	}))
+})
