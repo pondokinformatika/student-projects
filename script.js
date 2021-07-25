a5349cd64c05c592d178dabc6ba37984a33914bb
AbidKhairyAK <abid.khairy.ak@gmail.com>
2021-07-25 21:19:37 +0700

    init

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 document.addEventListener('alpine:init', () => {
-	Alpine.data('main', () => ({
-		students: [
-			{ name: 'muji', projects: [
-				{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
-			]}, 
-			{ name: 'abdu', projects: [
-				{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
-			]}, 
-			{ name: 'fatra', projects: [
-				{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
-			]}, 
-			{ name: 'hudzaifah', projects: [
-				{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
-			]}, 
-			{ name: 'ramadhan', projects: [
-				{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
-			]}, 
-			{ name: 'ihsan', projects: [
-				{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
-			]}, 
-			{ name: 'asep', projects: [
-				{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
-			]},
-		],
-	}))
+	Alpine.data('main', () => {
+		const data = {
+			students: [
+				{ name: 'muji', projects: [
+					{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
+				]}, 
+				{ name: 'abdu', projects: [
+					{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
+				]}, 
+				{ name: 'fatra', projects: [
+					{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
+				]}, 
+				{ name: 'hudzaifah', projects: [
+					{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
+				]}, 
+				{ name: 'ramadhan', projects: [
+					{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
+				]}, 
+				{ name: 'ihsan', projects: [
+					{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
+				]}, 
+				{ name: 'asep', projects: [
+					{ name: 'E Learning', path: 'e-learning', image: 'https://profesi-unm.com/wp-content/uploads/2020/03/ewww.jpg' }
+				]},
+			],
+		}
+
+		const isLocal = window.location.href.split('://')[0] === 'file'
+		const paths = window.location.pathname.split('/')
+		const dirName = paths[paths.length - (isLocal ? 2 : 1)]
+		const studentDetail = data.students.find(student => student.name === dirName)
+		const dataFiltered = studentDetail || data
+
+		return { ...dataFiltered }
+	})
 })
